304eb00f37090fa164e97e0f49409abf483b18a9
melisaozdoyuran <melisaozdoyuran@hotmail.com>
2023-12-12 04:41:17 -0500

    fixes and updates

--- Desktop/portfolio/app.js
+++ Desktop/portfolio/app.js
@@ -1,54 +1,55 @@
-// Import necessary modules and configuration
-require('dotenv').config(); // Loads environment variables from a .env file
-const express = require("express"); // Express framework for building web applications
-const path = require("path"); // Provides utilities for working with file and directory paths
-const exphbs = require("express-handlebars"); // Handlebars view engine for Express
-const bodyParser = require("body-parser"); // Middleware to parse incoming request bodies
-const projects = require("./projects.json"); // Import a JSON file containing project data
-const nodemailer = require('nodemailer'); // Module to send emails easily
-const sgTransport = require('nodemailer-sendgrid-transport'); // Transport layer for Nodemailer to send emails through SendGrid
+require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const projects = require("./projects.json");
+const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 
-const app = express(); // Initialize an Express application
+const app = express();
 
 // Body parser setup
-app.use(bodyParser.urlencoded({ extended: true })); // Parses URL-encoded bodies (like HTML forms)
-app.use(bodyParser.json()); // Parses JSON bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Static files setup
-app.use(express.static(path.join(__dirname, "/public"))); // Serves static files from the /public directory
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.static('public'));
 
 // Handlebars setup with custom helper
 const hbs = exphbs.create({
-    extname: ".hbs", // Set the file extension for views to .hbs
-    defaultLayout: false, // Disables default layout
+    extname: ".hbs",
+    defaultLayout: false,
     helpers: {
-        eq: (a, b) => a === b // Custom helper 'eq' for comparison in templates
+        eq: (a, b) => a === b
     }
 });
+
+app.engine('.hbs', hbs.engine);
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "views"));
 
 // Nodemailer with SendGrid setup
 const options = {
     auth: {
-        api_key: process.env.SENDGRID_API_KEY // Authentication with SendGrid using an API key from the environment variables
+        api_key: process.env.SENDGRID_API_KEY
     }
 }
-const transporter = nodemailer.createTransport(sgTransport(options)); // Create a Nodemailer transporter using SendGrid
+const transporter = nodemailer.createTransport(sgTransport(options));
 
 // Routes
-// Define routes for different URLs, each rendering a specific Handlebars view
 app.get("/", (req, res) => {
-    res.render("home"); // Renders the home view
+    res.render("home");
 });
 
 app.get("/contact", (req, res) => {
-    res.render("contact", { submitted: "no" }); // Renders the contact view with a variable
+    res.render("contact", { submitted: "no" });
 });
 
 app.get("/work", (req, res) => {
-    res.render("work", { projects: projects }); // Renders the work view with projects data
+    res.render("work", { projects: projects });
 });
-
 
 app.get("/about", (req, res) => {
     res.render("about");
@@ -62,46 +63,43 @@ app.get("/fun", (req, res) => {
     res.render("fun");
 });
 
-app.get("/games", (req, res) => {
-    res.render("games");
-});
-
 app.get("/project/:pid([0-9]+)", (req, res) => {
     var pid = req.params.pid;
     var thisProj = projects[pid.toString()];
     res.render("project", { project: thisProj });
 });
 
-
-// Handle POST request on the contact form
 app.post("/contact", (req, res) => {
-    const { fullname, email, subject, note } = req.body; // Extract data from the request body
+    const { fullname, email, subject, note } = req.body;
 
     let mailOptions = {
-        from: process.env.EMAIL_USER, // Sender address from environment variables
-        to: "melisaozdoyuran@hotmail.com", // Hardcoded recipient address
-        subject: `Contact Form - ${subject}`, // Subject line with a template literal
+        from: process.env.EMAIL_USER,
+        to: "melisaozdoyuran@hotmail.com",
+        subject: `Contact Form - ${subject}`,
         text: `
 From: ${fullname} (${email})
 Subject: ${subject}
 
 ${note}
-        ` // Construct the email body
+        `
     };
 
     transporter.sendMail(mailOptions, (err, info) => {
         if (err) {
             console.error("Error sending email:", err);
-            res.render("contact", { submitted: "error" }); // Render contact view with error status
+            res.render("contact", { submitted: "error" });
         } else {
             console.log("Email sent:", info.response);
             console.log("Email send response:", info);
 
-            res.render("contact", { submitted: "yes" }); // Render contact view with success status
+            res.render("contact", { submitted: "yes" });
         }
     });
 });
 
-const port = process.env.PORT || 8080; // Set the port from environment variables or default to 8080
-app.listen(port); // Start the server on the specified port
-console.log("Express started. Listening on port %s", port); // Log message indicating server has started
+  
+  
+
+const port = process.env.PORT || 8080;
+app.listen(port); 
+console.log("Express started. Listening on port %s", port);
